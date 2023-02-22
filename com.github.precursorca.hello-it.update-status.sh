7f4c62602827572447c228736b75cfb73f5587ad
Alex Narvey <anarvey@precursor.ca>
2023-02-22 11:44:14 -0600

    Update com.github.precursorca.hello-it.update-status.sh
    Removed doing an actual software update check in favour of polling the software update plist already on the computer. Same great taste, less time consuming!

--- com.github.precursorca.hello-it.update-status.sh
+++ com.github.precursorca.hello-it.update-status.sh
@@ -13,36 +13,28 @@
 #  Created by Alex Narvey, Precursor Systems
 #
 #  Written: 02/16/23
+#  Modified:  02/22/23
 
 
 ### The following line load the Hello IT bash script lib
 . "$HELLO_IT_SCRIPT_SH_LIBRARY/com.github.ygini.hello-it.scriptlib.sh"
 
+updatestate=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate.plist LastRecommendedUpdatesAvailable)
 
-#POSITIVE="No new software available."
-LOC="/Users/Shared/update_status.txt"
-softwareupdate --list 2>$LOC
-STATUS="$(cat $LOC)"
-#echo $STATUS
-if [ "${STATUS}" = "No new software available." ]
-    then 
-    echo "No macOS updates for you!"
-    else
-    echo "macOS updates are available for your computer."
-fi
 
 function updateStatus {
-    if [ "${STATUS}" = "No new software available." ] # No new software available
+    if [ "${updatestate}" -eq 0 ]
     then
         updateTitle "macOS is up-to-date!"
         updateState "${STATE[0]}"
-        updateTooltip '"macOS is patched and up-to-date."'
-	else # macOS update(s) are available. 
+        updateTooltip "macOS is patched and up-to-date"
+    else
         updateTitle "macOS has an update waiting."
         updateState "${STATE[1]}"
         updateTooltip "Please update using System Settings: Software Udpate"
     fi
 }
+
 
 ### This function is called when the user click on your item
 function onClickAction {
